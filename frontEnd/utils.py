8a12af51280381fb36f4ed09d95e58fe33f34980
sepulveda272 <sepulvedarojasjuandavid@gmail.com>
2024-12-16 22:30:11 -0500

    feat: :sparkles: falta documentacion

--- frontEnd/utils.py
+++ frontEnd/utils.py
@@ -13,13 +13,19 @@ def import_tasks(filename):
     session = Session()
     with open(filename, 'r') as f:
         tasks_list = json.load(f)
+
     for task_data in tasks_list:
-        task = Task(
-            title=task_data['title'],
-            description=task_data['description'],
-            completed=task_data['completed']
-        )
-        session.add(task)
+        existing_task = session.query(Task).filter_by(title=task_data['title']).first()
+        if not existing_task:
+            task = Task(
+                title=task_data['title'],
+                description=task_data['description'],
+                completed=task_data['completed']
+            )
+            session.add(task)
+        else:
+            print(f"La tarea '{task_data['title']}' ya existe.")
+    
     session.commit()
     session.close()
 
